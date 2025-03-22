4fd1549a658cecc70a963e0f5645b52c411d11d0
sdfthsrthterh <1410726455@qq.com>
2025-03-22 22:48:32 +0800

    print snakes

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,5 +1,6 @@
 import { AcGameObject } from "./AcGameObject";
 import { Wall } from "./Wall";
+import { Sanke } from "./Sanke";
 
 export class GameMap extends AcGameObject {
     constructor(ctx, parent) {
@@ -10,10 +11,16 @@ export class GameMap extends AcGameObject {
         this.L = 0;  //格子的绝对长度
 
         this.rows = 13;
-        this.cols = 13;
+        this.cols = 14;
         
         this.inner_walls_count = parseInt(Math.random() * 90); //内部障碍物数量
         this.walls = [];
+
+        this.snakes = [
+            new Sanke({id: 0, color: "#8EC0E4", r: this.rows - 2, c: 1}, this),
+            new Sanke({id: 1, color: "#ed317f", r: 1, c: this.cols - 2}, this),
+        ];
+
     }
 
     //洪水灌溉：从起点搜到终点
@@ -53,7 +60,7 @@ export class GameMap extends AcGameObject {
             g[0][c] = g[this.rows - 1][c] = true;
         }
 
-
+ 
 
         //创建随机障碍物
         for(let i = 0; i < this.inner_walls_count / 2; i++)
@@ -66,7 +73,7 @@ export class GameMap extends AcGameObject {
                 //起始的两端不放
                 if(c == 1 &&  r == this.rows - 2 || r == 1 && c == this.cols - 2) continue;
                 //对称放置
-                g[r][c] = g[c][r] = true; 
+                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;   //中心坐标对称变换
                 break;
             }
         }
@@ -89,12 +96,29 @@ export class GameMap extends AcGameObject {
 
     }
 
+    add_listening_events() {
+        this.ctx.canvas.focus();
+
+        const [snake0, snake1] = this.snakes;
+        this.ctx.canvas.addEventListener("keydown", e=> {
+            if(e.key === 'w') snake0.set_direction(0);
+            else if(e.key === 'd') snake0.set_direction(1);
+            else if(e.key === 's') snake0.set_direction(2);
+            else if(e.key === 'a') snake0.set_direction(3);
+            else if(e.key === 'ArrowUp') snake1.set_direction(0);
+            else if(e.key === 'ArrowRight') snake1.set_direction(1);
+            else if(e.key === 'ArrowDown') snake1.set_direction(2);
+            else if(e.key === 'ArrowLeft') snake1.set_direction(3);
+        });
+    }
+
     start() {
         for(let i = 0; i < 1000; i++) {
             if(this.create_walls()) {  //如果找到合法路径则直接break
                 break;
             }
         }
+        this.add_listening_events();
     }
 
     update_size() {
@@ -104,8 +128,45 @@ export class GameMap extends AcGameObject {
         this.ctx.canvas.height = this.L * this.rows;
     }
 
+    check_ready() {   //判断两条蛇是否都准备好进入下一个回合了 
+        for(const snake of this.snakes) {
+            if(snake.status !== "idle") return false;
+            if(snake.direction === -1) return false;
+        }
+        return true;
+    }
+
+    next_step() {   //让两条蛇进入下一个回合
+        for(const snake of this.snakes) {
+            snake.next_step();  //调用snake的下一步函数
+        }
+    }
+
+    check_valid(cell) { //检测目标位置是否合法：没有撞到墙和两条蛇的身体部分
+        for (const wall of this.walls) {
+            if(wall.r === cell.r && wall.c === cell.c) {
+                return false;
+            }
+        }
+
+        for (const snake of this.snakes) {
+            let k = snake.cells.length;
+            if(!snake.check_tail_increasing()) {  //如果蛇尾会前进的时候，蛇尾不要判断
+                k--;
+            }
+            for (let i = 0; i < k; i++) {
+                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c)
+                return false;
+            }
+        }
+        return true;
+    }
+
     update() {
         this.update_size();
+        if(this.check_ready()) {  //如果确认两条蛇都准备好了，那么进入下一步
+            this.next_step();
+        }
         this.render();
     }
 
